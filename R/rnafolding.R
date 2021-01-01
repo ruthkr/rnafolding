a7a6c6631869db9b3dd7cf6a1ad82e09dd74accd
Ruth Kristianingsih <ruth.kristianingsih30@gmail.com>
2021-01-01 20:10:35 +0000

    Added package-level documentation

---
+++ R/rnafolding.R
@@ -0,0 +1,11 @@
+#' \code{rnafolding} package
+#'
+#' See the README on
+#' \href{https://github.com/ruthkr/rnafolding/}{GitHub}
+#'
+#' @docType package
+#' @name rnafolding
+NULL
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
